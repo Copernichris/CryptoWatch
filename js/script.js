6e722cbe3ecd06175e1d4c0d9a6c37e5b88f7e94
Chase Nicks <chasenicks81@gmail.com>
2021-07-17 13:28:19 -0400

    got MVP version of news api working

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 var tickerEl = document.getElementById("capValue");
+var newsTitleEl = document.getElementById("title-1");
+var newDescrEl = document.getElementById("news-content-1");
+var newsTitleEl2 = document.getElementById("title-2");
+var newDescrEl2 = document.getElementById("news-content-2");
 
 fetch("https://api.nomics.com/v1/currencies/ticker?key=bf0f8b8bff53b4098df1df96d4d2d0531a4d8ffa&ids=BTC&interval=1d,30d&convert=EUR&per-page=100&page=1")
   .then(response => response.json())
@@ -19,3 +23,19 @@ fetch("https://api.nomics.com/v1/currencies/ticker?key=bf0f8b8bff53b4098df1df96d
     return newValue;
   
   }
+
+fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol=BTC&limit=2&sources=news")
+  .then (response => response.json())
+  .then (data => newsTitleEl.innerText = data.data[0].title)
+
+fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol=BTC&limit=2&sources=news")
+  .then (response => response.json())
+  .then (data => newDescrEl.innerText = data.data[0].description)
+
+fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol=BTC&limit=2&sources=news")
+  .then (response => response.json())
+  .then (data => newsTitleEl2.innerText = data.data[1].title)
+
+fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol=BTC&limit=2&sources=news")
+  .then (response => response.json())
+  .then (data => newDescrEl2.innerText = data.data[1].description)
