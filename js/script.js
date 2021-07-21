935aed1d82bd9830ca8f4d169b4fcd87d31aa33c
Leland Hayes <lelandkhayes13@gmail.com>
2021-07-21 18:57:31 -0400

    added chart code

--- js/script.js
+++ js/script.js
@@ -34,31 +34,74 @@ fetch("https://api.nomics.com/v1/currencies/ticker?key=bf0f8b8bff53b4098df1df96d
   .then(response => response.json())  
   .then(data => console.log(data))
 
- function abbreviateNumber(value) {
-    let newValue = value;
-    const suffixes = ["", "K", "M", "B","T"];
-    let suffixNum = 0;
-    while (newValue >= 1000) {
-      newValue /= 1000;
-      suffixNum++;
-    }
-  
-    newValue = newValue.toPrecision(3);
-  
-    newValue += suffixes[suffixNum];
-    return newValue;
-  
+function abbreviateNumber(value) {
+  let newValue = value;
+  const suffixes = ["", "K", "M", "B","T"];
+  let suffixNum = 0;
+  while (newValue >= 1000) {
+    newValue /= 1000;
+    suffixNum++;
   }
+  
+  newValue = newValue.toPrecision(3);
+  
+  newValue += suffixes[suffixNum];
+  return newValue;
+}
 
-fetch("https://api.nomics.com/v1/exchange-rates/history?key=bf0f8b8bff53b4098df1df96d4d2d0531a4d8ffa&ids=BTC&start=2021-07-15T01:33:52+00:00&end=2021-07-20T01:33:52+00:00&format=json")
-  .then(response => response.json())
-  .then(data => console.log(data))
-
-  TESTER = document.getElementById('charts');
-	Plotly.newPlot( TESTER, [{
-	x: [1, 2, 3, 4, 5],
-	y: [1, 2, 4, 8, 16] }], {
-	margin: { t: 0 } } );
+var xmlhttp = new XMLHttpRequest();
+  var api = 'https://api.lunarcrush.com/v2?data=assets&key=axnpldsftoa03n17z75cy5r&symbol=BTC&interval=day&time_series_indicators=open,close,high,low&data_points=50';
+  xmlhttp.open("GET", api, true);
+  xmlhttp.send();
+  xmlhttp.onreadystatechange = function(){
+      if(this.readyState == 4 && this.status == 200){
+            var data = JSON.parse(this.responseText);
+            var closes = data.data[0].timeSeries.map(function(elem){
+                return elem.close;
+            });
+            var dates = data.data[0].timeSeries.map(function(elem){
+                var date = new Date(elem.time *1000);
+                var formattedDate = (date.getMonth()+1)+
+                "/"+date.getDate()+
+                 "/"+date.getFullYear();
+                return formattedDate;
+            });
+            var opens = data.data[0].timeSeries.map(function(elem){
+                return elem.open;
+            });
+            var ctx = document.getElementById('MyChart').getContext('2d');
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: dates,
+                    datasets: [{
+                        label: 'open',
+                        data: opens,
+                        backgroundColor: '#2B6FFF',
+                        borderColor: '#2B6FFF',
+                        borderWidth: 2,
+                        pointStyle: 'rectRot',
+  
+                    },{
+                        label: 'close',
+                        data: closes,
+                        backgroundColor: 'black',
+                        borderColor: 'black',
+                        borderWidth: 2,
+                        pointStyle: 'rectRot'
+                    }
+                ]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: false
+                        }
+                    }
+                }
+            });
+  }
+}
 
 //Variable for charts data. Placeholders in place of where api's data would be put in
   var charts = [
