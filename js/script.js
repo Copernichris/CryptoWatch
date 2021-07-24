40ae8071b0ae08590124222fe05dc293bafa5be5
Leland Hayes <lelandkhayes13@gmail.com>
2021-07-24 11:51:36 -0400

    dont merge

--- js/script.js
+++ js/script.js
@@ -74,8 +74,8 @@ function displayLatestSearches() {
 
         recentButtonEl.on("click", (event) => {
           saveSearchTerm(event.target.id);
-          // $(chartObject)(event.target.id);
-          // $(chartObjectTwo)(event.target.id);
+          R(event.target.id);
+          // chartObjectTwo(event.target.id);
 
 
       })
@@ -96,138 +96,143 @@ function saveSearchTerm(searchTerm) {
   localStorage.setItem("coinsearch", JSON.stringify(prevTerms));
 }
 
+// function that pulls the latest data
 function loadLatestSearch() {
 
   var latestTerms = pullLatestSearches();
 
   var recentlySearched = latestTerms[0];
 
-  // searchHandler(recentlySearched);
+  recentlyButtonRender(recentlySearched);
 }
 
-// chart fix
-var chartObject = {};
-var chartObjectTwo = {};  
-
-// adding event listener to the button
-function searchHandler() {
+function recentButtonRender () {
+  // event listener to render charts/news
   $('#submitBtn').on("click", () => {
-    saveSearchTerm($('#search').val());    
-    // $(chartObject)($('#search').val());
-    // $(chartObjectTwo)($('#search').val());
+    saveSearchTerm($('#search').val()); 
+    managingData($('#search').val());       
+
     var tokenKey;        
     tokenKey = ($('#search').val());
 
-    fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
+// renders chart after each search is made
+var chartObject = {};
+var chartObjectTwo = {};  
+
+// Runs all api data 
+function managingData(recentButtonRender) {
+// news data
+fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
   .then (response => response.json())
   .then (data => newsTitleEl.innerText = data.data[0].title)
 
-  fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
-    .then (response => response.json())
-    .then (data => newDescrEl.innerText = data.data[0].description)
+fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
+  .then (response => response.json())
+  .then (data => newDescrEl.innerText = data.data[0].description)
 
-  fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
-    .then (response => response.json())
-    .then (data => newsTitleEl2.innerText = data.data[1].title)
+fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
+  .then (response => response.json())
+  .then (data => newsTitleEl2.innerText = data.data[1].title)
 
-  fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
-    .then (response => response.json())
-    .then (data => newDescrEl2.innerText = data.data[1].description)
+fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
+  .then (response => response.json())
+  .then (data => newDescrEl2.innerText = data.data[1].description)
 
-  fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
-    .then (response => response.json())
-    .then (data => newsTitleEl3.innerText = data.data[2].title)
+fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
+  .then (response => response.json())
+  .then (data => newsTitleEl3.innerText = data.data[2].title)
 
-  fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
-    .then (response => response.json())
-    .then (data => newDescrEl3.innerText = data.data[2].description)
+fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
+  .then (response => response.json())
+  .then (data => newDescrEl3.innerText = data.data[2].description)
 
-  fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
-    .then (response => response.json())
-    .then (data => newsTitleEl4.innerText = data.data[3].title)
+fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
+  .then (response => response.json())
+  .then (data => newsTitleEl4.innerText = data.data[3].title)
 
-  fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
-    .then (response => response.json())
-    .then (data => newDescrEl4.innerText = data.data[3].description)
+fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
+  .then (response => response.json())
+  .then (data => newDescrEl4.innerText = data.data[3].description)
 
-  fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
-    .then (response => response.json())
-    .then (data => newsTitleEl5.innerText = data.data[4].title)
+fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
+  .then (response => response.json())
+  .then (data => newsTitleEl5.innerText = data.data[4].title)
 
-  fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
-    .then (response => response.json())
-    .then (data => newDescrEl5.innerText = data.data[4].description)
+fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
+  .then (response => response.json())
+  .then (data => newDescrEl5.innerText = data.data[4].description)
   
-  fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
-    .then (response => response.json())
-    .then (data => newsTitleEl6.innerText = data.data[5].title)
+fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
+  .then (response => response.json())
+  .then (data => newsTitleEl6.innerText = data.data[5].title)
   
-  fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
-    .then (response => response.json())
-    .then (data => newDescrEl6.innerText = data.data[5].description)
+fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
+  .then (response => response.json())
+  .then (data => newDescrEl6.innerText = data.data[5].description)
 
-  fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
-    .then (response => response.json())
-    .then (data => newsTitleEl7.innerText = data.data[6].title)
+fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
+  .then (response => response.json())
+  .then (data => newsTitleEl7.innerText = data.data[6].title)
   
-  fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
-    .then (response => response.json())
-    .then (data => newDescrEl7.innerText = data.data[6].description)
+fetch("https://api.lunarcrush.com/v2?data=feeds&key=axnpldsftoa03n17z75cy5r&symbol="+tokenKey+"&limit=10&sources=news")
+  .then (response => response.json())
+  .then (data => newDescrEl7.innerText = data.data[6].description)
 
-    var xmlhttp = new XMLHttpRequest();
-    var api = 'https://api.lunarcrush.com/v2?data=assets&key=axnpldsftoa03n17z75cy5r&symbol='+tokenKey+'&interval=day&time_series_indicators=open,close,high,low&data_points=90';
-    xmlhttp.open("GET", api, true);
-    xmlhttp.send();
-    xmlhttp.onreadystatechange = function(){
-         if(this.readyState == 4 && this.status == 200){
-              var data = JSON.parse(this.responseText);
-              var closes = data.data[0].timeSeries.map(function(elem){
-                  return elem.close;
-              });
-              var dates = data.data[0].timeSeries.map(function(elem){
-                  var date = new Date(elem.time *1000);
-                  var formattedDate = (date.getMonth()+1)+
-                  "/"+date.getDate()+
-                   "/"+date.getFullYear();
-                  return formattedDate;
-              });
-              var opens = data.data[0].timeSeries.map(function(elem){
-                  return elem.open;
-              });
-              var ctx = document.getElementById('MyChart').getContext('2d');
-              if(chartObject.chart instanceof Chart) {
-                chartObject.chart.destroy();
-              }
-              chartObject.chart = new Chart(ctx, {
-                  type: 'line',
-                  data: {
-                      labels: dates,
-                      datasets: [{
-                          label: 'open',
-                          data: opens,
-                          backgroundColor: '#2B6FFF',
-                          borderColor: '#2B6FFF',
-                          borderWidth: 2,
-                          pointStyle: 'rectRot',
+  // chart render/data
+  var xmlhttp = new XMLHttpRequest();
+  var api = 'https://api.lunarcrush.com/v2?data=assets&key=axnpldsftoa03n17z75cy5r&symbol='+tokenKey+'&interval=day&time_series_indicators=open,close,high,low&data_points=90';
+  xmlhttp.open("GET", api, true);
+  xmlhttp.send();
+  xmlhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            var data = JSON.parse(this.responseText);
+            var closes = data.data[0].timeSeries.map(function(elem){
+                return elem.close;
+            });
+            var dates = data.data[0].timeSeries.map(function(elem){
+                var date = new Date(elem.time *1000);
+                var formattedDate = (date.getMonth()+1)+
+                "/"+date.getDate()+
+                "/"+date.getFullYear();
+                return formattedDate;
+            });
+            var opens = data.data[0].timeSeries.map(function(elem){
+                return elem.open;
+            });
+            var ctx = document.getElementById('MyChart').getContext('2d');
+            if(chartObject.chart instanceof Chart) {
+              chartObject.chart.destroy();
+            }
+            chartObject.chart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: dates,
+                    datasets: [{
+                        label: 'open',
+                        data: opens,
+                        backgroundColor: '#2B6FFF',
+                        borderColor: '#2B6FFF',
+                        borderWidth: 2,
+                        pointStyle: 'rectRot',
     
-                      },{
-                          label: 'close',
-                          data: closes,
-                          backgroundColor: 'black',
-                          borderColor: 'black',
-                          borderWidth: 2,
-                          pointStyle: 'rectRot'
-                      }
-                  ]
-                  },
-                  options: {
-                      scales: {
-                          y: {
-                              beginAtZero: false
-                          }
-                      }
-                  }
-              });
+                    },{
+                        label: 'close',
+                        data: closes,
+                        backgroundColor: 'black',
+                        borderColor: 'black',
+                        borderWidth: 2,
+                        pointStyle: 'rectRot'
+                    }
+                ]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: false
+                        }
+                    }
+                }
+            });
      }
   }  
 
@@ -280,37 +285,34 @@ function searchHandler() {
    }
 }  
 
-  //nomics
   //mktcap
 fetch("https://api.nomics.com/v1/currencies/ticker?key=bf0f8b8bff53b4098df1df96d4d2d0531a4d8ffa&ids="+tokenKey+"&interval=1d,30d&convert=EUR&per-page=100&page=1")
-.then(response => response.json())  
-.then(data => marketCapEl.innerText = abbreviateNumber(data[0].market_cap))
+  .then(response => response.json())  
+  .then(data => marketCapEl.innerText = abbreviateNumber(data[0].market_cap))
 
 //volume
 fetch("https://api.nomics.com/v1/currencies/ticker?key=bf0f8b8bff53b4098df1df96d4d2d0531a4d8ffa&ids="+tokenKey+"&interval=1d,30d&convert=EUR&per-page=100&page=1")
-.then(response => response.json())  
-.then(data => volumeEl.innerText = abbreviateNumber(data[0]["1d"].volume))
+  .then(response => response.json())  
+  .then(data => volumeEl.innerText = abbreviateNumber(data[0]["1d"].volume))
 
 //supply
 fetch("https://api.nomics.com/v1/currencies/ticker?key=bf0f8b8bff53b4098df1df96d4d2d0531a4d8ffa&ids="+tokenKey+"&interval=1d,30d&convert=EUR&per-page=100&page=1")
-.then(response => response.json())  
-.then(data => supplyEl.innerText = abbreviateNumber(data[0].circulating_supply))
-
-//test call
-fetch("https://api.nomics.com/v1/currencies/ticker?key=bf0f8b8bff53b4098df1df96d4d2d0531a4d8ffa&ids="+tokenKey+"&interval=1d,30d&convert=EUR&per-page=100&page=1")
-.then(response => response.json())  
-.then(data => console.log(data))
+  .then(response => response.json())  
+  .then(data => supplyEl.innerText = abbreviateNumber(data[0].circulating_supply))
+    }
   })
 }
 
 // final running of functions
 function init() {
 
-  loadLatestSearch();
-
   displayLatestSearches();
 
-  searchHandler();
+  loadLatestSearch();
+
+  managingData();
+
+  recentButtonRender ();
 }
 
 init();
