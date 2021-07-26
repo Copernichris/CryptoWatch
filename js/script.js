f47452e65f5716d843ba10a03a7094b6a48a71dc
Leland Hayes <lelandkhayes13@gmail.com>
2021-07-26 18:18:57 -0400

    fix nomics fetch

--- js/script.js
+++ js/script.js
@@ -317,7 +317,6 @@ fetch("https://api.nomics.com/v1/currencies/ticker?key=bf0f8b8bff53b4098df1df96d
 fetch("https://api.nomics.com/v1/currencies/ticker?key=bf0f8b8bff53b4098df1df96d4d2d0531a4d8ffa&ids="+tokenKey+"&interval=1d,30d&convert=EUR&per-page=100&page=1")
 .then(response => response.json())  
 .then(data => supplyEl.innerText = abbreviateNumber(data[0].circulating_supply))
-
 }
 
 // final running of functions
